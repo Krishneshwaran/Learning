f48b3d3057b7c206dbca2106ea7d90475108038c
Krishneshwaran <krishneshwarand@gmail.com>
2024-10-13 00:35:21 +0530

    Added Typing Functionality

--- src/main.asm
+++ src/main.asm
@@ -2,6 +2,8 @@ org 0x7C00
 bits 16
 
 %define ENDL 0x0D, 0x0A
+%define ENTER_KEY 0x0D
+%define BACKSPACE_KEY 0x08
 
 start:
     jmp main
@@ -54,6 +56,47 @@ putch:
     pop ax
     ret
 
+;
+; Read a line of input into a buffer
+; Params:
+;   - ds:di points to buffer
+;
+getline:
+    push di
+    mov cx, 0           ; character count
+
+.read_loop:
+    call get_key        ; get key from user
+    cmp al, ENTER_KEY   ; check if Enter is pressed
+    je .done            ; if Enter, we're done
+
+    cmp al, BACKSPACE_KEY ; check if Backspace is pressed
+    je .backspace
+
+    ; store character in buffer
+    stosb               ; store al in buffer
+    call putch          ; display character
+    inc cx              ; increase character count
+    jmp .read_loop
+
+.backspace:
+    cmp cx, 0           ; if there's something to delete
+    je .read_loop       ; if buffer is empty, ignore
+    dec di              ; move pointer back in buffer
+    dec cx              ; reduce character count
+    mov al, ' '         ; overwrite the character on screen with space
+    call putch
+    mov al, BACKSPACE_KEY ; move cursor back
+    call putch
+    jmp .read_loop
+
+.done:
+    ; null-terminate the input
+    mov al, 0
+    stosb
+    pop di
+    ret
+
 main:
     ; setup data segments
     mov ax, 0
@@ -72,17 +115,26 @@ main:
     mov si, msg_prompt
     call puts
 
-    ; read input and print it
-    call get_key
-    call putch
+    ; buffer for user input
+    mov di, input_buffer
+    call getline
+
+    ; display the typed command
+    mov si, msg_you_typed
+    call puts
+    mov si, input_buffer
+    call puts
 
     hlt
 
 .halt:
     jmp .halt
 
-msg_hello:  db 'Hello! Press any key: ', ENDL, 0
-msg_prompt: db 'You pressed: ', 0
+msg_hello:      db 'Welcome to My OS!', ENDL, 0
+msg_prompt:     db 'Type something: ', 0
+msg_you_typed:  db ENDL, 'You typed: ', 0
+
+input_buffer:   times 64 db 0         ; buffer for input (64 bytes)
 
 times 510-($-$$) db 0
 dw 0xAA55
